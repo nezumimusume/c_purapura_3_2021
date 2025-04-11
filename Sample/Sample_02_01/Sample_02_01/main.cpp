983271fb42c7ac94778077adaeadbb54e7ff1c4a
TakayukiKiyohara <nezumimusume@gmail.com>
2025-04-12 05:02:53 +0900

    std::vectorのサンプルを追加

---
+++ Sample/Sample_02_01/Sample_02_01/main.cpp
@@ -0,0 +1,27 @@
+﻿// ConsoleApplication1.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+//step-1 vectorをインクルード
+
+int main()
+{
+    //step-2 int型の要素を記録できる可変長配列を定義する。
+
+    //step-3 hogeに要素を追加する
+
+    //step-4 hogeのサイズを表示する。
+    
+    //step-5 for文を使って、hogeの要素の値を表示する。
+
+    //step-6 さらにhogeに要素を追加する。
+    
+    //step-7 再度hogeのサイズを表示する。
+    
+    //step-8 もう一度for文を使って、hogeの要素の値を表示する。
+
+    return 0;
+}
+
+
+
